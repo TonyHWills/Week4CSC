fd28d3506f3d2f6e5d135e5803beee75375cf595
TonyHWills <xo7583gd@go.minnstate.edu>
2023-02-09 12:51:02 -0600

    first commit

---
+++ Controllers/HWills_Week3Api.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HWills_CS2_Week3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HWills_Week3Api : ControllerBase
+    {
+        [HttpPost(Name = "GetWeatherForecast")]
+        public ActionResult<List<string>> IntListWork(List<int> lint)
+        {
+            List<string> stringyList = new List<string>();
+            double count = 0;
+            double sum = 0;
+            double sumSq = 0;
+            
+            double standDev;
+
+            lint.Sort();
+            try{
+               foreach (int i in lint){
+                    if (count <= 0)
+                    {
+                        count++;
+                        sum += i;
+                        sumSq += i * i;
+                        standDev = Math.Sqrt((sumSq - sum * sum / count) / (count - 1));
+                        stringyList.Add("List too small");
+                    }
+                    else
+                    {
+                        count++;
+                        sum += i;
+                        sumSq += i * i;
+                        standDev = Math.Sqrt((sumSq - sum * sum / count) / (count - 1));
+                        stringyList.Add(count + ": Current S.D.: " + standDev);
+                    }
+               }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong boss!");
+
+            }
+            Console.WriteLine("Sum: " + sum);
+
+            return Accepted(stringyList);
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace HWills_CS2_Week3
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
